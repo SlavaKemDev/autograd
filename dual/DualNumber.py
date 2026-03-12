34a955e6f2d40c1b77e04bc712b055c3bd9c7746
Вячеслав Чертан <slavakemdev@gmail.com>
2026-03-12 23:19:06 +0300

    Add DualNumber class with basic arithmetic operations

---
+++ dual/DualNumber.py
@@ -0,0 +1,62 @@
+import math
+
+class DualNumber:
+    def __init__(self, real, dual):
+        self.real = real
+        self.dual = dual
+
+    def __add__(self, other):
+        if isinstance(other, DualNumber):
+            return DualNumber(self.real + other.real, self.dual + other.dual)
+        else:
+            return DualNumber(self.real + other, self.dual)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __mul__(self, other):
+        if isinstance(other, DualNumber):
+            return DualNumber(self.real * other.real, self.real * other.dual + self.dual * other.real)
+        else:
+            return DualNumber(self.real * other, self.dual * other)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __repr__(self):
+        if self.dual == 0:
+            return f"{self.real}"
+        if self.real == 0:
+            return f"{self.dual}e"
+        if self.dual > 0:
+            return f"{self.real} + {self.dual}e"
+        else:
+            return f"{self.real} - {-self.dual}e"
+
+    def __pow__(self, power, modulo=None):
+        A = self.real
+        B = self.dual
+        C = power.real if isinstance(power, DualNumber) else power
+        D = power.dual if isinstance(power, DualNumber) else 0
+
+        return DualNumber(
+            A ** C,
+            D * (A ** C) * math.log(A) + (A ** (C - 1)) * B * C
+        )
+
+    def __rpow__(self, other):
+        return DualNumber(other, 0).__pow__(self)
+
+    def __truediv__(self, other):
+        A = self.real
+        B = self.dual
+        C = other.real if isinstance(other, DualNumber) else other
+        D = other.dual if isinstance(other, DualNumber) else 0
+
+        return DualNumber(
+            A / C,
+            B / C - (A * D) / (C * C)
+        )
+
+    def __rtruediv__(self, other):
+        return DualNumber(other, 0).__truediv__(self)
